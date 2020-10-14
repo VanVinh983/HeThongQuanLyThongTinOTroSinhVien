e64e13f291846251d215675a7b4c22ead2fc0ee1
HTC <doanvinh983@gmail.com>
2020-10-14 16:31:46 +0700

    update all project

---
+++ HeThongQuanLyNhaTro/src/connectDB/ConnectDB.java
@@ -0,0 +1,35 @@
+package connectDB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectDB {
+	private static Connection con = null;
+	private static ConnectDB instance = new ConnectDB();
+
+	public static Connection getConnecction() {
+		return con;
+	}
+
+	public static ConnectDB getInstance() {
+		return instance;
+	}
+
+	public void connect() throws SQLException {
+
+		String url = "jdbc:sqlserver://localhost:1433;databasename=";
+		String user = "sa";
+		String password = "sapassword";
+		con = DriverManager.getConnection(url, user, password);
+	}
+
+	public void disconnect() {
+		if (con != null)
+			try {
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+	}
+}
