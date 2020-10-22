8c689729eaed149ed477f178115b47c0c50ac88e
FPT SHOP <FPT SHOP@172.16.205.217>
2020-10-22 13:05:10 +0700

    update class ui

---
+++ HeThongQuanLyNhaTro/src/ui/GiaoDienQLyTro.java
@@ -0,0 +1,323 @@
+package ui;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.HeadlessException;
+import java.awt.Insets;
+import java.awt.Label;
+import java.awt.LayoutManager;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
+import javax.swing.table.DefaultTableModel;
+
+public class GiaoDienQLyTro extends JFrame{
+
+	private JTextField txtMaNhatro;
+	private JTextField txtChuNha;
+	private JTextField txtDiaChi;
+	private JTextField txtSDT;
+	private JTextField txtTim;
+	private JLabel lblMaNhaTro;
+	private JLabel lblChuNha;
+	private JLabel lblDiaChi;
+	private JLabel lblSDT;
+	private JLabel lblTim;
+	private JTextField txtUser;
+	private JButton btnTim;
+	private JButton btnThem;
+	private JButton btnXoa;
+	private JButton btnSua;
+	private JButton btnXoaTrang;
+	private JLabel lblUser;
+	private JLabel lblcen1;
+	private DefaultTableModel tableModel;
+	private JTable table;
+	private JLabel lblThongTin;
+	private JButton btnTro;
+	private JButton btnSinhVien;
+	private JButton btnThongKe;
+	private JButton btnThueTro;
+	private JButton btnNhanVien;
+	private JButton btnHuongDanSD;
+	private JButton btnThoat;
+	
+	private JComboBox<String> cmp;
+	public GiaoDienQLyTro() {
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		//setLocationRelativeTo(null);
+		setTitle("Quan Ly Thong Tin Tro");
+		setSize(1550, 860);
+		
+		JPanel pnl = new JPanel();
+		pnl.setLayout(new BorderLayout());
+		Box box = Box.createVerticalBox();
+		Box b1, b2;
+		box.add(b1 = Box.createHorizontalBox());
+		
+			JPanel pnluser1 = new JPanel();
+				pnluser1.setBackground(Color.GREEN);
+				Box buser = Box.createVerticalBox();
+					Box buser1, buser2, buser3;
+					buser.add(buser1 = Box.createHorizontalBox());
+						buser1.add(lblUser = new JLabel("User: "));
+						lblUser.setFont(new Font("Arial", Font.BOLD, 35));
+						buser1.add(txtUser = new JTextField(20));
+						buser1.add(Box.createVerticalStrut(20));
+						txtUser.setEnabled(false);
+					buser.add(buser2 = Box.createVerticalBox());
+	
+					//Hinh Anh
+					JPanel pnlcontent=new JPanel();
+		     		JLabel lblBanner = new JLabel();
+					pnlcontent.add(lblBanner);
+					lblBanner.setSize(250,250);
+					add(pnlcontent);
+					setPicture(lblBanner, "img/User.png");
+					
+					buser2.add(pnlcontent);
+				pnluser1.add(buser);
+				
+				pnluser1.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY));
+		b1.add(pnluser1);
+		pnl.add(box, BorderLayout.WEST);
+		add(pnl);
+		
+		/////////////////////////////////////////////////
+		
+		JPanel pnluser2 = new JPanel();
+		pnluser2.setLayout(new BorderLayout());
+			JPanel pnluser22 = new JPanel();
+			pnluser22.setBackground(Color.MAGENTA);
+			pnluser2.add(pnluser22, BorderLayout.CENTER);
+			pnluser22.setLayout(new BoxLayout(pnluser22, BoxLayout.Y_AXIS));
+				box.add(pnluser2);
+				
+				//Box bQlyTro = Box.createVerticalBox();
+				pnluser22.add(Box.createVerticalStrut(20));
+				pnluser22.add(btnTro = new JButton("Quan Ly Tro"));
+				
+				btnTro.add(Box.createHorizontalStrut(30));
+				btnTro.add(Box.createVerticalStrut(20));
+				pnluser22.add(Box.createVerticalStrut(10));
+				pnluser22.add(btnSinhVien = new JButton("Quan Sinh Vien"));
+				
+				btnSinhVien.add(Box.createHorizontalStrut(30));
+				btnSinhVien.add(Box.createVerticalStrut(20));
+				pnluser22.add(Box.createVerticalStrut(10));
+				pnluser22.add(btnNhanVien = new JButton("Quan Ly Nhan Vien"));
+				
+				btnNhanVien.add(Box.createHorizontalStrut(30));
+				btnNhanVien.add(Box.createVerticalStrut(20));
+				pnluser22.add(Box.createVerticalStrut(10));
+				pnluser22.add(btnThueTro = new JButton("Quan Ly Thue tro"));
+				
+				btnThueTro.add(Box.createHorizontalStrut(30));
+				btnThueTro.add(Box.createVerticalStrut(20));
+				pnluser22.add(Box.createVerticalStrut(10));
+				pnluser22.add(btnThongKe = new JButton("Thong Ke"));
+				
+				btnThongKe.add(Box.createHorizontalStrut(30));
+				btnThongKe.add(Box.createVerticalStrut(20));
+				pnluser22.add(Box.createVerticalStrut(10));
+				pnluser22.add(btnHuongDanSD = new JButton("Huong Dan Su Dung"));
+				
+				btnHuongDanSD.add(Box.createHorizontalStrut(30));
+				btnHuongDanSD.add(Box.createVerticalStrut(20));
+				pnluser22.add(Box.createVerticalStrut(10));
+				pnluser22.add(btnThoat = new JButton("Thoat"));
+				btnThoat.add(Box.createHorizontalStrut(30));
+				btnThoat.add(Box.createVerticalStrut(20));
+		
+		//Center
+		JPanel pnlCen = new JPanel();
+		Box boxCen = Box.createVerticalBox();
+		
+		Box bcen1, bcen2, bcen3;
+		boxCen.add(bcen1 = Box.createHorizontalBox());
+		
+		JPanel pnlQuanLyTro = new JPanel();
+		pnlQuanLyTro.add(lblcen1 = new JLabel("Quản lý nhà trọ"));
+		bcen1.add(pnlQuanLyTro);
+		pnlQuanLyTro.setBackground(Color.blue);
+		lblcen1.setFont(new Font("Arial", Font.BOLD, 40));
+		lblcen1.setForeground(Color.YELLOW);
+	//Table
+		boxCen.add(bcen2 = Box.createHorizontalBox());
+		
+		
+		JPanel pnlcen2 = new JPanel();
+			JScrollPane scroll;
+			String[] header="Mã nhà trọ;Tên chủ nhà;Địa chỉ;Số điện thoại".split(";");
+			tableModel=new DefaultTableModel(header,20);
+			pnlcen2.add(scroll=new JScrollPane(table=new JTable(tableModel),JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED),BorderLayout.SOUTH);
+			scroll.setBorder(BorderFactory.createTitledBorder("Danh Sách"));
+			table.setRowHeight(50);
+			scroll.setPreferredSize(new Dimension(0,500));
+			bcen2.add(scroll);
+		///////////////////
+		pnl.add(boxCen, BorderLayout.CENTER);
+		
+		// South
+		boxCen.add(bcen3 = Box.createVerticalBox());
+		JPanel pnlcen3 = new JPanel();
+		pnlcen3.setLayout(new BorderLayout());
+		
+		Box boxcen31, boxcen32;
+		pnlcen3.add(boxcen31 = box.createVerticalBox(), BorderLayout.NORTH);
+
+		bcen3.add(pnlcen3);
+		JPanel pnlThongTin = new JPanel();
+		pnlThongTin.add(lblThongTin = new JLabel("Thông tin"));
+		boxcen31.add(pnlThongTin);
+		lblThongTin.setFont(new Font("Arial", Font.BOLD, 30));
+		pnlThongTin.setBackground(Color.LIGHT_GRAY);
+		
+		
+		
+
+		///////////////////////////////////////////
+	
+		bcen3.add(boxcen32 = Box.createVerticalBox());
+		JPanel pnlSouth = new JPanel();
+		pnlSouth.setLayout(new BorderLayout());
+		pnlSouth.setBackground(Color.LIGHT_GRAY);
+		
+		pnlSouth.add(Box.createHorizontalStrut(100), BorderLayout.WEST);
+		JPanel pnlForm=new JPanel();
+		pnlSouth.add(pnlForm);
+		boxcen31.add( pnlSouth);
+		pnlForm.setBounds(0,80,1055,450);
+		pnlForm.setLayout(new BoxLayout(pnlForm, BoxLayout.Y_AXIS));
+		
+		
+		pnlForm.add(Box.createVerticalStrut(30));
+		Box boxMaTro = Box.createHorizontalBox();
+		boxMaTro.add(lblMaNhaTro=new JLabel("Mã Tro:"));
+		boxMaTro.add(txtMaNhatro=new JTextField());
+		pnlForm.add(boxMaTro);
+		
+		pnlForm.add(Box.createVerticalStrut(10));
+		Box boxDiaChi = Box.createHorizontalBox();
+		boxDiaChi.add(lblDiaChi=new JLabel("Địa chỉ: "));
+		boxDiaChi.add(txtDiaChi=new JTextField());
+		pnlForm.add(boxDiaChi);
+		
+		pnlForm.add(Box.createVerticalStrut(10));
+		Box boxChuNha = Box.createHorizontalBox();
+		boxChuNha.add(lblChuNha=new JLabel("Tên chu tro"));
+		boxChuNha.add(txtChuNha=new JTextField());
+		pnlForm.add(boxChuNha);
+		
+		pnlForm.add(Box.createVerticalStrut(10));
+		Box boxSDT = Box.createHorizontalBox();
+		boxSDT.add(lblSDT=new JLabel("Số điện thoại"));
+		boxSDT.add(txtSDT=new JTextField());
+		pnlForm.add(boxSDT);
+		
+		pnlForm.add(Box.createVerticalStrut(10));
+		Box boxButton = Box.createHorizontalBox();
+		boxButton.add(btnThem =new JButton("Them"));
+		boxButton.add(Box.createHorizontalStrut(20));
+		boxButton.add(btnXoa =new JButton("Xoa"));
+		boxButton.add(Box.createHorizontalStrut(20));
+		boxButton.add(btnSua =new JButton("Sua"));
+		boxButton.add(Box.createHorizontalStrut(20));
+		boxButton.add(btnXoaTrang =new JButton("Xoa Trang"));
+		pnlForm.add(boxButton);
+		pnlForm.add(Box.createVerticalStrut(20));
+		
+		
+		
+		
+		 pnlForm.setBorder(BorderFactory.createRaisedBevelBorder());
+		 
+		 JPanel pnlSouthRight = new JPanel();
+		 pnlSouthRight.setLayout(new BoxLayout(pnlSouthRight, BoxLayout.Y_AXIS));
+		 pnlSouth.add(pnlSouthRight, BorderLayout.EAST);
+		 pnlSouthRight.setBorder(BorderFactory.createLoweredSoftBevelBorder());
+		 
+		 
+		 pnlSouthRight.add(Box.createVerticalStrut(30));
+		 Box boxtxtTim = Box.createHorizontalBox();
+		 boxtxtTim.add(lblTim=new JLabel("Tim"));
+		 boxtxtTim.add(txtTim=new JTextField(20));
+		 pnlSouthRight.add(boxtxtTim);
+		 
+		 pnlSouthRight.add(Box.createVerticalStrut(30));
+		 Box boxcmpTim = Box.createHorizontalBox();
+		 String[] s = "ma;Ten;diachi,sdt".split(";");
+		 boxcmpTim.add(cmp = new JComboBox<String>(s));
+		 pnlSouthRight.add(boxcmpTim);
+		 
+		 pnlSouthRight.add(Box.createVerticalStrut(30));
+		 Box boxbtnTim = Box.createHorizontalBox();
+		 boxbtnTim.add(btnTim = new JButton("Tim"));
+		 btnTim.add(Box.createHorizontalStrut(50));
+		 btnTim.add(Box.createVerticalStrut(50));
+		 btnTim.setBackground(Color.green);
+		 pnlSouthRight.setBackground(Color.CYAN);
+		 pnlSouthRight.add(boxbtnTim);
+		 
+		 
+		lblMaNhaTro.setPreferredSize(lblSDT.getPreferredSize());
+		lblChuNha.setPreferredSize(lblSDT.getPreferredSize());
+		lblDiaChi.setPreferredSize(lblSDT.getPreferredSize());
+		lblTim.setPreferredSize(lblSDT.getPreferredSize());
+	
+		
+		
+		
+		
+				/////////////////////////////////////////////////
+		
+		//pnlcen3.add(Box.createHorizontalStrut(400), BorderLayout.EAST);
+
+		setVisible(true);
+	}
+	
+	public  void setPicture(  JLabel label ,String filename ){
+        try {
+          BufferedImage image = ImageIO.read(new File(filename));
+          int x =label.getSize().width;
+          int y =label.getSize().height;
+          int ix =image.getWidth();
+          int iy =image.getHeight();
+
+          int dx=x;
+          int dy=y;
+         
+
+          ImageIcon icon = new ImageIcon(image.getScaledInstance(dx, dy, BufferedImage.SCALE_SMOOTH));
+          label.setIcon(icon);
+      } catch (IOException ex) {
+    	  JOptionPane.showMessageDialog(null, "Loi hinh anh");
+      }
+
+  }
+	
+	public static void main(String[] args) {
+		new GiaoDienQLyTro(); 
+	}
+}
---
+++ HeThongQuanLyNhaTro/src/ui/Nhap.java
@@ -0,0 +1,92 @@
+package ui;
+
+public class Nhap {
+
+	//Box boxcen321, boxcen322, boxcen323,boxcen324, boxcen325,boxcen326;
+	
+	//Box boxleft, boxright;
+	//boxleft.add(comp)
+//	Box boxcen32left, boxcen32right;
+//	boxcen32.add(boxcen32left = Box.createVerticalBox());
+	
+	
+//	boxcen32left.add(boxcen321 = Box.createHorizontalBox());
+////	boxcen321.add(Box.createVerticalStrut(10));
+//	boxcen321.add(lblMaNhaTro = new JLabel("Mã tro:"));
+//	boxcen321.add(txtMaNhatro=new JTextField());
+////	boxcen32left.add(Box.createVerticalStrut(20));
+//	boxcen32left.add(boxcen322 = Box.createHorizontalBox());
+//	boxcen322.add(lblDiaChi = new JLabel("Địa chỉ: "));
+//	boxcen322.add(txtDiaChi=new JTextField());
+//	boxcen32left.add(Box.createVerticalStrut(20));
+//	
+//	
+//	boxcen32.add(boxcen323 = Box.createHorizontalBox());
+//	boxcen323.add(lblChuNha = new JLabel("chu nha: "));
+//	boxcen323.add(txtChuNha=new JTextField());
+//	//boxcen32.add(Box.createVerticalStrut(20));
+//	boxcen32.add(boxcen324 = Box.createHorizontalBox());
+//	boxcen324.add(lblSDT = new JLabel("Số điện thoại:"));
+//	boxcen324.add(txtSDT=new JTextField());
+//	//boxcen32.add(Box.createVerticalStrut(20));
+//	boxcen32.add(boxcen325 = Box.createHorizontalBox());
+//	boxcen325.add(lblTim = new JLabel("Tim kiem:"));
+//	boxcen325.add(txtTim = new JTextField());
+//	String[] s = "ma;Ten;diachi,sdt".split(";");
+//	boxcen325.add(Box.createHorizontalStrut(10));
+//	boxcen325.add(cmp = new JComboBox<String>(s));
+//	
+//	boxcen325.add(btnTim = new JButton("Tim"));
+//	//boxcen32.add(Box.createVerticalStrut(20));
+//	
+//	boxcen32.add(boxcen326 = Box.createHorizontalBox());
+//	boxcen326.add(btnThem =new JButton("Them"));
+//	boxcen326.add(Box.createHorizontalStrut(20));
+//	boxcen326.add(btnXoa =new JButton("Xoa"));
+//	boxcen326.add(Box.createHorizontalStrut(20));
+//	boxcen326.add(btnSua =new JButton("Sua"));
+//	boxcen326.add(Box.createHorizontalStrut(20));
+//	boxcen326.add(btnXoaTrang =new JButton("XoaTrang"));
+//	boxcen326.add(Box.createHorizontalStrut(20));
+//	boxcen32.add(Box.createVerticalStrut(20));
+//	///////////////////
+//	lblMaNhaTro.setPreferredSize(lblSDT.getPreferredSize());
+//	lblChuNha.setPreferredSize(lblSDT.getPreferredSize());
+//	lblDiaChi.setPreferredSize(lblSDT.getPreferredSize());
+//	
+//	lblTim.setPreferredSize(lblSDT.getPreferredSize());
+//	
+	
+//	
+//	JPanel pnlbtn = new JPanel();
+//	pnlbtn.setLayout(new BoxLayout(pnlbtn, BoxLayout.Y_AXIS));
+//	pnlcen3.add(pnlbtn, BorderLayout.EAST);
+//	Box boxThem, boxXoa, boxSua, boxXoaTrang;
+//	pnlbtn.add(Box.createVerticalStrut(10));
+//	pnlbtn.add(btnThem =new JButton("Them"));
+//	btnThem.add(Box.createHorizontalStrut(180));
+//	pnlbtn.add(Box.createVerticalStrut(10));
+//	pnlbtn.add(btnXoa =new JButton("Xoa"));
+//	btnXoa.add(Box.createHorizontalStrut(180));
+//	pnlbtn.add(Box.createVerticalStrut(10));
+//	pnlbtn.add(btnSua =new JButton("Sua"));
+//	btnSua.add(Box.createHorizontalStrut(180));
+//	pnlbtn.add(Box.createVerticalStrut(10));
+//	pnlbtn.add(btnXoaTrang =new JButton("Xoa Trang"));
+//	btnXoaTrang.add(Box.createHorizontalStrut(180));
+//	pnlbtn.add(Box.createVerticalStrut(10));
+//	pnlbtn.setPreferredSize(new Dimension(100,180));
+//	
+//	pnlbtn.setBorder(BorderFactory.createLineBorder(Color.RED));
+//	btnXoaTrang.setPreferredSize(new Dimension(0,80));
+//	btnThem.setPreferredSize(btnXoaTrang.getPreferredSize());
+//	btnXoa.setPreferredSize(btnXoaTrang.getPreferredSize());
+//	btnSua.setPreferredSize(btnXoaTrang.getPreferredSize());
+	
+//	pnlcen1.setBackground(Color.CYAN);
+//	bcen1.add(pnlcen1);
+//	bcen1.add(Box.)
+	//lblcen1.setPreferredSize(new Dimension(500,-10));
+	
+
+}
