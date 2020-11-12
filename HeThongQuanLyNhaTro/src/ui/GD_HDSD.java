c6aea86a3080f511725b7bbcbcad926c8898e8a3
DELL <DELL@192.168.1.56>
2020-11-12 21:06:11 +0700

    Updates

--- HeThongQuanLyNhaTro/src/ui/GD_HDSD.java
+++ HeThongQuanLyNhaTro/src/ui/GD_HDSD.java
@@ -17,29 +17,6 @@ public class GD_HDSD  extends JFrame{
 	
 	public GD_HDSD() {
 		
-		setBackground(Color.MAGENTA);
-		JPanel pNorth = new JPanel();
-		pNorth.add(lblTitle = new JLabel("HƯỚNG DẪN SỬ DỤNG"));
-		lblTitle.setForeground(Color.black);
-		Font font = new Font("Arias", Font.BOLD, 30);
-		lblTitle.setFont(font);
-		add(pNorth, BorderLayout.NORTH);
 		
-		Box b= Box.createVerticalBox();
-		Box b1 = Box.createHorizontalBox();
-		b.add(b1);
-		add(b,BorderLayout.CENTER);
-		
-		JLabel lblMaTro;
-		JTextField txtMaTro;
-		b1.add(txtMaTro = new  JTextField("oseph Robinette Biden, Jr. (còn gọi"
-				+ " là Joe Biden; /ˌrɒbɪˈnɛt ˈbaɪdən/[2]; sinh ngày 20 tháng "
-				+ "11 năm 1942) là Tổng thống tân cử của Hoa Kỳ. Sau khi đán"
-				+ "h bại tổng thống đương nhiệm Donald Trump trong cuộc bầu cử tổ"
-				+ "ng thống năm 2020, ông sẽ nhậm chức trở thành tổng thống thứ 46"
-				+ " vào tháng 1 năm 2021. Là thành viên của Đảng Dân chủ, ông từng"
-				+ " là Phó Tổng thống thứ 47 của Hoa Kỳ từ năm 2009 đến năm 2017. Ô"
-				+ "ng cũng từng là Thượng nghị sĩ Hoa Kỳ đại diện tiểu bang Dela"
-				+ "ware từ năm 1973 đến năm 2009."));
 }
 }
--- HeThongQuanLyNhaTro/src/ui/GD_ThemTroMoi.java
+++ HeThongQuanLyNhaTro/src/ui/GD_ThemTroMoi.java
@@ -2,177 +2,213 @@ package ui;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.time.LocalDate;
+import java.awt.Label;
 
 import javax.swing.Box;
-import javax.swing.JButton;
-import javax.swing.JFrame;
+import javax.swing.BoxLayout;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JSplitPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
 
-import entity.NhanVien;
+public class GD_ThemTroMoi extends JPanel{
 
-public class GD_ThemTroMoi extends JFrame implements ActionListener, MouseListener{
 
-	private JLabel lblTitle,lblMaTro,lblDiaChi,lblTenChuTro,lblSDT;
-	private JTextField txtDiaChi, txtTenChuTro,txtSDT, txtMaTro;
-	private JButton  btnLuu,btnThoat,btnThem, btnXoaTrang;
-	
-
-	public GD_ThemTroMoi(){
+	private DefaultTableModel tableModel;
+	private JTable table;
+	private DefaultTableModel tableModelBTT;
+	private JTable tableBTT;
+	private JTextField txtSDT;
+	private JTextField txtTPTenSinhVien;
+	private JTextField txtTPMaNhaTro;
+	private JTextField txtTPTenChuNhaTro;
+	private JTextField txtGiaThue;
+	private JTextField txtNgayCapNhat;
+	private JTextField txtNgayBatDau;
+	private JTextField txtNgayKetThuc;
+	public GD_ThemTroMoi() {
+		setAutoscrolls(true);
+		setBorder(null);
+		this.setPreferredSize(new Dimension(1400, 350));
+		setLayout(new BorderLayout(0, 0));
 		
-		setBackground(Color.MAGENTA);
-		JPanel pNorth = new JPanel();
-		pNorth.add(lblTitle = new JLabel("THÊM TRỌ MỚI"));
-		lblTitle.setForeground(Color.black);
-		Font font = new Font("Arias", Font.BOLD, 30);
-		lblTitle.setFont(font);
-		add(pNorth, BorderLayout.NORTH);
-		
-		Box b= Box.createVerticalBox();
-		Box b1 = Box.createHorizontalBox();
-		Box b2 = Box.createHorizontalBox();
-		Box b3 = Box.createHorizontalBox();
-		Box b4 = Box.createHorizontalBox();
-		
-		
-		b.add(b1);
-		b.add(Box.createVerticalStrut(10));
-		b.add(b2);
-		b.add(Box.createVerticalStrut(10));
-		b.add(b3);
-		b.add(Box.createVerticalStrut(10));
-		b.add(b4);
-		b.add(Box.createVerticalStrut(10)); 
-		b.add(Box.createVerticalStrut(10));
-		add(b,BorderLayout.CENTER);
+		JPanel pnlRight = new JPanel();
+		pnlRight.setMinimumSize(new Dimension(1000, 10));
+		pnlRight.setPreferredSize(new Dimension(100, 10));
+		add(pnlRight, BorderLayout.CENTER);
+		pnlRight.setLayout(new BorderLayout(0, 0));
 		
 		
-		b1.add(lblMaTro = new JLabel("Mã Trọ:"));
-		b1.add(txtMaTro = new  JTextField(10));
+		JPanel pnlNhapThongTin = new JPanel();
+		pnlNhapThongTin.setBackground(Color.CYAN);
+		pnlNhapThongTin.setPreferredSize(new Dimension(1400, 280));
+		pnlRight.add(pnlNhapThongTin, BorderLayout.EAST);
+		pnlNhapThongTin.setLayout(new BorderLayout(0, 0));
 		
-		b2.add(lblDiaChi = new JLabel("Địa Chỉ:"));
-		b2.add(txtDiaChi = new JTextField(10));
+		JPanel pnlNhapThongTinCoBan = new JPanel();
+		pnlNhapThongTin.add(pnlNhapThongTinCoBan, BorderLayout.CENTER);
+		pnlNhapThongTinCoBan.setLayout(new BorderLayout(0, 0));
 		
-		b3.add(lblTenChuTro = new JLabel("Tên Chủ Trọ:"));
-		b3.add(txtTenChuTro = new JTextField(10));
+		JPanel pnlTieuDeNhapThongTin = new JPanel();
+		pnlTieuDeNhapThongTin.setForeground(Color.YELLOW);
+		pnlTieuDeNhapThongTin.setPreferredSize(new Dimension(10, 50));
+		pnlTieuDeNhapThongTin.setBackground(Color.GRAY);
+		pnlNhapThongTinCoBan.add(pnlTieuDeNhapThongTin, BorderLayout.NORTH);
 		
 		
-		b4.add(lblSDT = new JLabel("Số Điện Thoại: "));
-		b4.add(txtSDT = new JTextField(10));
+		JPanel pnlKhoangTrongTrai = new JPanel();
+		pnlKhoangTrongTrai.setBackground(Color.GRAY);
+		pnlKhoangTrongTrai.setPreferredSize(new Dimension(50, 10));
+		pnlNhapThongTinCoBan.add(pnlKhoangTrongTrai, BorderLayout.WEST);
+		
+		JLabel lblNhapThongTin = new JLabel("Thêm Trọ Mới");
+		lblNhapThongTin.setForeground(Color.WHITE);
+		lblNhapThongTin.setFont(new Font("Arial", Font.BOLD, 27));
+		pnlTieuDeNhapThongTin.add(lblNhapThongTin);
+		
+		JPanel pnlThanhPhan = new JPanel();
+		pnlNhapThongTinCoBan.add(pnlThanhPhan, BorderLayout.CENTER);
+		pnlThanhPhan.setLayout(new BoxLayout(pnlThanhPhan, BoxLayout.Y_AXIS));
+		
+		JPanel pnlTPNhaTro = new JPanel();
+		pnlThanhPhan.add(Box.createVerticalStrut(10));
+		pnlThanhPhan.add(pnlTPNhaTro);
+		pnlTPNhaTro.setLayout(new BoxLayout(pnlTPNhaTro, BoxLayout.X_AXIS));
+		
+		Component horizontalStrut_2 = Box.createHorizontalStrut(20);
+		pnlTPNhaTro.add(horizontalStrut_2);
+		
+		JLabel lblTPMaNhaTro = new JLabel("Mã Nhà Trọ: ");
+		lblTPMaNhaTro.setFont(new Font("Arial", Font.PLAIN, 15));
+		pnlTPNhaTro.add(lblTPMaNhaTro);
+		
+		txtTPMaNhaTro = new JTextField();
+		txtTPMaNhaTro.setFont(new Font("Arial", Font.PLAIN, 15));
+		pnlTPNhaTro.add(txtTPMaNhaTro);
+		txtTPMaNhaTro.setColumns(10);
+		
+		Component horizontalStrut_3 = Box.createHorizontalStrut(20);
+		horizontalStrut_3.setPreferredSize(new Dimension(30, 0));
+		pnlTPNhaTro.add(horizontalStrut_3);
+		
+		JLabel lblTenChuNha = new JLabel("Tên Chủ Nhà: ");
+		lblTenChuNha.setFont(new Font("Arial", Font.PLAIN, 15));
+		pnlTPNhaTro.add(lblTenChuNha);
+		
+		txtTPTenChuNhaTro = new JTextField();
+		txtTPTenChuNhaTro.setFont(new Font("Arial", Font.PLAIN, 15));
+		pnlTPNhaTro.add(txtTPTenChuNhaTro);
+		txtTPTenChuNhaTro.setColumns(10);
+		
+		Component horizontalStrut_9 = Box.createHorizontalStrut(20);
+		pnlTPNhaTro.add(horizontalStrut_9);
+		
+		JPanel pnlTPSinhVien = new JPanel();
+		pnlThanhPhan.add(Box.createVerticalStrut(10));
+		pnlThanhPhan.add(pnlTPSinhVien);
+		pnlTPSinhVien.setLayout(new BoxLayout(pnlTPSinhVien, BoxLayout.X_AXIS));
+		
+		Component horizontalStrut_1 = Box.createHorizontalStrut(20);
+		pnlTPSinhVien.add(horizontalStrut_1);
+		
+		JLabel lblSDT = new JLabel("SDT Chủ Nhà: ");
+		lblSDT.setFont(new Font("Arial", Font.PLAIN, 15));
+		pnlTPSinhVien.add(lblSDT);
+		
+		txtSDT = new JTextField();
+		txtSDT.setPreferredSize(new Dimension(20, 20));
+		txtSDT.setFont(new Font("Arial", Font.PLAIN, 15));
+		pnlTPSinhVien.add(txtSDT);
+		txtSDT.setColumns(10);
+		
+		Component horizontalStrut = Box.createHorizontalStrut(20);
+		horizontalStrut.setPreferredSize(new Dimension(30, 0));
+		pnlTPSinhVien.add(horizontalStrut);
 		
 		
-		lblMaTro.setPreferredSize(lblSDT.getPreferredSize());
-		lblDiaChi.setPreferredSize(lblSDT.getPreferredSize());
-		lblTenChuTro.setPreferredSize(lblSDT.getPreferredSize());
-	
 		
-		 
-		JSplitPane split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
-			add(split, BorderLayout.SOUTH);
-			JPanel pEast;	
-			
-			split.add(pEast = new JPanel());
-			pEast.add(btnThem = new JButton("Thêm"));
-			pEast.add(btnLuu = new JButton("Lưu"));
-			pEast.add(btnThoat = new JButton("Thoát"));
-			pEast.add(btnXoaTrang = new JButton("Xóa Trắng"));
-			setSize(1000, 350);
-			setVisible(true);
-			
-			setDefaultCloseOperation(HIDE_ON_CLOSE);
-			
-			btnThem.addActionListener(this);
+		Component horizontalStrut_10 = Box.createHorizontalStrut(20);
+		pnlTPSinhVien.add(horizontalStrut_10);
 		
-			btnXoaTrang.addActionListener(this);
-			btnLuu.addActionListener(this);
-			
-			setLocationRelativeTo(null);
-			setResizable(false);
-			
-			
-			
+		JPanel pnlTPDiaChiTro = new JPanel();
+		pnlThanhPhan.add(Box.createVerticalStrut(10));
+		pnlThanhPhan.add(pnlTPDiaChiTro);
+		pnlTPDiaChiTro.setLayout(new BoxLayout(pnlTPDiaChiTro, BoxLayout.X_AXIS));
+		
+		Component horizontalStrut_7 = Box.createHorizontalStrut(20);
+		pnlTPDiaChiTro.add(horizontalStrut_7);
+		
+		JLabel lblTPDiaChiTro = new JLabel("Địa Chỉ: ");
+		lblTPDiaChiTro.setFont(new Font("Arial", Font.PLAIN, 15));
+		pnlTPDiaChiTro.add(lblTPDiaChiTro);
+		
+		JLabel lblQuan = new JLabel("Quận:\r\n \r\n");
+		lblQuan.setFont(new Font("Arial", Font.PLAIN, 15));
+		pnlTPDiaChiTro.add(lblQuan);
+		
+		JComboBox cboQuan = new JComboBox();
+		cboQuan.setFont(new Font("Arial", Font.PLAIN, 15));
+		pnlTPDiaChiTro.add(cboQuan);
+		
+		Component horizontalStrut_4 = Box.createHorizontalStrut(20);
+		pnlTPDiaChiTro.add(horizontalStrut_4);
+		
+		JLabel lblNewLabel = new JLabel("Phường: \r\n");
+		lblNewLabel.setFont(new Font("Arial", Font.PLAIN, 15));
+		pnlTPDiaChiTro.add(lblNewLabel);
+		
+		JComboBox cboPhuong = new JComboBox();
+		cboPhuong.setFont(new Font("Arial", Font.PLAIN, 15));
+		pnlTPDiaChiTro.add(cboPhuong);
+		
+		Component horizontalStrut_5 = Box.createHorizontalStrut(20);
+		pnlTPDiaChiTro.add(horizontalStrut_5);
+		
+		JLabel lblDuong = new JLabel("Đường: ");
+		lblDuong.setFont(new Font("Arial", Font.PLAIN, 15));
+		pnlTPDiaChiTro.add(lblDuong);
+		
+		JComboBox cboDuong = new JComboBox();
+		cboDuong.setFont(new Font("Arial", Font.PLAIN, 15));
+		pnlTPDiaChiTro.add(cboDuong);
+		
+		Component horizontalStrut_6 = Box.createHorizontalStrut(20);
+		pnlTPDiaChiTro.add(horizontalStrut_6);
+		
+		JLabel lblSoNha = new JLabel("Số Nhà: ");
+		lblSoNha.setFont(new Font("Arial", Font.PLAIN, 15));
+		pnlTPDiaChiTro.add(lblSoNha);
+		
+		JComboBox cboSoNha = new JComboBox();
+		cboSoNha.setFont(new Font("Arial", Font.PLAIN, 15));
+		pnlTPDiaChiTro.add(cboSoNha);
+		
+		Component horizontalStrut_8 = Box.createHorizontalStrut(20);
+		pnlTPDiaChiTro.add(horizontalStrut_8);
+		
+		JPanel pnlGia_NgayCapNhat = new JPanel();
+		pnlThanhPhan.add(Box.createVerticalStrut(10));
+		pnlThanhPhan.add(pnlGia_NgayCapNhat);
+		pnlGia_NgayCapNhat.setLayout(new BoxLayout(pnlGia_NgayCapNhat, BoxLayout.X_AXIS));
+		
+		Component horizontalStrut_11 = Box.createHorizontalStrut(20);
+		pnlGia_NgayCapNhat.add(horizontalStrut_11);
+		
+		JLabel lblGia = new JLabel("Giá Thuê: ");
+		lblGia.setFont(new Font("Arial", Font.PLAIN, 15));
+		pnlGia_NgayCapNhat.add(lblGia);
+		
+		txtGiaThue = new JTextField();
+		txtGiaThue.setFont(new Font("Arial", Font.PLAIN, 15));
+		pnlGia_NgayCapNhat.add(txtGiaThue);
+		txtGiaThue.setColumns(10);
+		
+		lblTPMaNhaTro.setPreferredSize(lblSDT.getPreferredSize());
+		lblGia.setPreferredSize(lblSDT.getPreferredSize());
 	}
-	
-	private void xoaTrangAction() {
-		// TODO Auto-generated method stub
-		
-		
-		txtMaTro.setText("");
-		txtDiaChi.setText("");
-		txtSDT.setText("");
-		txtTenChuTro.setText("");
-		
-	}
-
-//	private void themAction() {
-//		// TODO Auto-generated method stub
-//		try {
-//			
-//			
-//			String maTro= txtMaTro.getText();
-//			String tenChuTro = txtTenChuTro.getText();
-//			String diaChi = txtDiaChi.getText();
-//			String SDT = txtSDT.getText();
-//			
-//			NhanVien nhanVien =  new NhanVien(maNhanVien, maPhongBan, tenNhanVien, LocalDate.of(namSinh, thangSinh, ngaySinh), gioiTinh, noiSinh, diaChi, email, soDienThoai);
-//			
-//			if(dao.themNhanVien(nhanVien)) {
-//				String []row = {maNhanVien, tenNhanVien, nhanVien.ngaySinhToString(),nhanVien.gioiTinhToString(), noiSinh, diaChi, email, soDienThoai,tenPhongBan, ""};
-//				tableModel.addRow(row);
-//				xoaTrangAction();
-//				JOptionPane.showMessageDialog(this, "Thêm thành công!");
-//			}
-//			else {
-//				JOptionPane.showMessageDialog(this, "Thêm thất bại!");
-//				txtMaNhanVien.selectAll();
-//				txtMaNhanVien.requestFocus();
-//			}
-//		}catch (Exception e) {
-//			// TODO: handle exception
-//			e.printStackTrace();
-//			JOptionPane.showMessageDialog(this, "Lỗi nhập liệu!");
-//		}
-//	}
-	@Override
-	public void mouseClicked(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void mouseEntered(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void mouseExited(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void mousePressed(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void mouseReleased(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	
-	
 }
