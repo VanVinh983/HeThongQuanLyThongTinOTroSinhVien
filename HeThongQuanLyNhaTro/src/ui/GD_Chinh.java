ea1dcf9fb3d6e26d3015a3fe57a19115b059b0dd
VANVINH <VANVINH@DESKTOP-9GFRHTI>
2020-12-08 20:22:29 +0700

    sửa lại các thư viện thìn làm lỗi

--- HeThongQuanLyNhaTro/src/ui/GD_Chinh.java
+++ HeThongQuanLyNhaTro/src/ui/GD_Chinh.java
@@ -3,9 +3,8 @@ package ui;
 import javax.swing.JFrame;
 
 public class GD_Chinh {
-	public static JFrame frame;
 	public static void main(String[] args) {
-		 frame = new JFrame();
+		JFrame frame = new JFrame();
 		frame.setTitle("Hệ Thống Quản Lý Ở Trọ Của Sinh Viên Trường Đại Học Công Nghiệp");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().add(new GD_DangNhap());
